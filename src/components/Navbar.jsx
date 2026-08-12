a0512816df9d7b4b38eb0f5d79c95eed561f7468
danielbilkvist <danielbilkvist@gmail.com>
2026-08-12 14:32:37 +0200

    navbar, css files and image upload

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,20 @@
 import { NavLink } from "react-router";
+import "./Navbar.css";
 
 function Navbar() {
   return (
     <header className="site-header">
       <NavLink className="brand" to="/">
-        Dit Navn
+        <img src={`${import.meta.env.BASE_URL}public/logo.svg`} alt="Hero" />
       </NavLink>
 
       <nav className="site-nav" aria-label="Primær navigation">
         <NavLink to="/" end>
-          Forside
+          Home
         </NavLink>
-        <NavLink to="/projects">Projekter</NavLink>
-        <NavLink to="/about">Om mig</NavLink>
-        <NavLink to="/contact">Kontakt</NavLink>
+        <NavLink to="/projects">Projects</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
       </nav>
     </header>
   );
