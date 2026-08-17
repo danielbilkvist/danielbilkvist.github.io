2e5da02791a6e72eec93174422dd3fdd9fbf58f5
danielbilkvist <danielbilkvist@gmail.com>
2026-08-17 17:13:36 +0200

    spelling mistake

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -12,7 +12,7 @@ function AboutPage() {
             {/* My name is Daniel Bentzen-Bilkvist, and I am a 23 year old designer and developer from Denmark. I have a passion for creating digital products that are both visually appealing and user-friendly. My goal is to create more than I consume, and I strive to make a positive impact through my work. */}
             My name is Daniel Bentzen-Bilkvist and I was born in 2003. I am
             currently living in Aarhus, Denmark, where i startet studying at
-            EAAA, to become a Multiumedia Designer by 2027. I love the concept
+            EAAA, to become a Multimedia Designer by 2027. I love the concept
             of bringing my ideas to life, wether its through coding a website,
             designing a poster or creating fictional stories.
           </p>
